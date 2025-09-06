dfd3887b78bde68e6804e59aa360c93af6c32809
Madekuji <27665110+Madekuji@users.noreply.github.com>
2025-09-06 23:57:41 +0800

    added sneak peek card and fixed projects colors

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 //import { Link } from "react-router-dom";
 import HomeDescription from "./components/HomeDescription.tsx";
-/*import Card from "./components/Card.tsx";
-import { Fragment } from "react";*/
+import Card from "./components/Card.tsx";
+import { Fragment } from "react";
 import MainButton from "./components/MainButton.tsx";
 import GoalTracker from "./components/GoalTracker.tsx";
 import FAQ from "./components/FAQ.tsx";
@@ -29,6 +29,20 @@ function Home() {
                     <Modal />
                 </div>
                 <GoalTracker/>
+                <Card text={
+                    <Fragment>
+                        <div className="cardHeader">
+                            <span className="cardTitle">Sneak Peek</span>
+                        </div>
+                        <img id="mainPhoto" src="/img/A4_POSTER_GROUP_RESIZE.webp" alt="Sneak peek for HoloFlasta 2025 standees"/>
+                        <p>
+                            <b>✨STAR!✨</b>
+                            <br/>
+                            <i>Anong suot nila?</i>
+                            <br/><br/>
+                        </p>
+                    </Fragment>
+                }/>
                 <FAQ/>
                 <div id="flasta-2024" className="card">
                     <p id="flasta-2024-content">
@@ -37,7 +51,7 @@ function Home() {
                         <br/><strong>Campaign Ended on October 6, 2024.</strong><br/>
                         HoloPro Fans PH was responsible for organizing the <strong>HoloFlasta 2024 Project</strong> in collaboration with other local hololive and HOLOSTARS community members as well as the donations of the community, which produced two flower stands dedicated to Anya Melfissa, Jurard T. Rexford, Octavio, and Vestia Zeta for Cosplay Mania 2024!<br/>
                     </p>
-                    <img id="flasta-2024-bg" src="/img/2024-flasta-bg.webp" />
+                    <img id="flasta-2024-bg" src="/img/2024-flasta-bg.webp" alt="HoloFlasta 2024 BG"/>
                 </div>
                 <ProjectsList/>
             </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,9 +3,7 @@ import type {ReactElement, JSXElementConstructor} from "react";
 function Card(props: { id?: string; style?: any; text: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>>; }) {
     return(
         <div id={props.id} className="card" style={props.style}>
-            <p>
                 {props.text}
-            </p>
         </div>
     );
 }
